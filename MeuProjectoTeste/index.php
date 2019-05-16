83d366f6cce2b391155d5d9cb4fe16241ef26267
Sergio <Sergio@PC-107>
2019-05-16 14:33:49 +0100

    quarto commit

--- MeuProjectoTeste/index.php
+++ MeuProjectoTeste/index.php
@@ -10,9 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        Modificação.
     </body>
 </html>
 <input type="submit" value="" />
